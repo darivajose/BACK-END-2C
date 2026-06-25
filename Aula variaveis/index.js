e45d6d40b69861cccd26889c681ec6cd3a11dad4
josedariva <167941121+darivajose@users.noreply.github.com>
2026-06-25 16:36:45 -0300

    variaveis

---
+++ Aula variaveis/index.js
@@ -0,0 +1,26 @@
+/* let forma = 'retangulo';
+let altura = 5;
+let comprimento = 7;
+let area;
+ 
+if (forma === 'retangulo'){
+    area = altura * comprimento;
+    } else {
+        area = (altura * comprimento) / 2;
+    }
+
+    console.log(area); */
+
+    //constante 
+const forma = 'quadrado';
+const altura = 5;
+const comprimento = 7;
+let area;
+if(forma === 'quadrado'){
+     area = altura * comprimento;
+
+} else {
+     area = (altura *comprimento) /2;
+}
+console.log(area);
+ 
